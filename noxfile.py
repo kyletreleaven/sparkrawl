6e380bba3aa5ccc87ec5a2123fae6d83738470e3
Kyle Treleaven <ktreleav@gmail.com>
2025-07-25 17:27:41 -0400

    preparing dev env for demo

--- noxfile.py
+++ noxfile.py
@@ -1,16 +1,27 @@
 import nox
 
+DEV = [
+    "pandas", "pyspark", "pysparkling",
+]
 
 @nox.session
 def dev(session):
-    session.install("pyspark", "ipython")
-    session.run("ipython")
+    session.install(*DEV, "ipython")
+
+    import os
+
+    # pyenv = ":".join(["src", "test_util", os.environ.get("PYTHONPATH", "")])
+
+    session.run(
+        "ipython",
+        env={"PYTHONPATH": "src:test_util"}
+    )
 
 
 @nox.session
 def test(session):
     session.install(
+        *DEV,
         "pytest", "pytest-coverage",
-        "pandas", "pyspark", "pysparkling",
     )
     session.run("pytest")
