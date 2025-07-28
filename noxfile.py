8c43de7aad17b2837044b1a16c62414b393edd41
Kyle Treleaven <ktreleav@gmail.com>
2025-07-28 15:19:18 -0400

    adding project spec

--- noxfile.py
+++ noxfile.py
@@ -1,27 +1,58 @@
 import nox
 
-DEV = [
-    "pandas", "pyspark", "pysparkling",
-]
+
+@nox.session
+def format(session):
+    """Runs the unit tests."""
+    try:
+        toml = nox.project.load_toml("pyproject.toml")
+        deps = toml["project"]["dependencies"]
+        build_deps = toml["project"]["optional-dependencies"]["build"]
+
+    except:
+        import json
+        print(json.dumps(toml, indent=2))
+        raise
+
+    session.install(*deps, *build_deps)  # but not the package itself...
+    session.run(
+        "black",
+        "src",
+    )
+
 
 @nox.session
 def dev(session):
-    session.install(*DEV, "ipython")
+    try:
+        toml = nox.project.load_toml("pyproject.toml")
+        dev_deps = toml["project"]["optional-dependencies"]["dev"]
 
-    import os
+    except:
+        import json
+        print(json.dumps(toml, indent=2))
+        raise
 
-    # pyenv = ":".join(["src", "test_util", os.environ.get("PYTHONPATH", "")])
+    session.install("-e", ".")
+    session.install(*dev_deps)
 
     session.run(
         "ipython",
-        env={"PYTHONPATH": "src:test_util"}
+        env={"PYTHONPATH": "test_util"}
     )
 
 
 @nox.session
 def test(session):
-    session.install(
-        *DEV,
-        "pytest", "pytest-coverage",
-    )
-    session.run("pytest")
+
+    try:
+        toml = nox.project.load_toml("pyproject.toml")
+        deps = toml["project"]["dependencies"]
+        test_deps = toml["project"]["optional-dependencies"]["test"]
+
+    except:
+        import json
+        print(json.dumps(toml, indent=2))
+        raise
+
+    session.install(*deps, *test_deps)
+    session.run("pytest", *(session.posargs or []))  # posargs for test filtering.
