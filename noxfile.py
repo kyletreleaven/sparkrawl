e2a0df869ff86a1ea7ab3979c8dcbf16d4b7c3c6
Kyle Treleaven <ktreleav@gmail.com>
2025-08-08 17:48:59 -0400

    adding build and doc environments to noxfile

--- noxfile.py
+++ noxfile.py
@@ -78,3 +78,35 @@ def test(session):
 
     session.install(*deps, *test_deps)
     session.run("pytest", *(session.posargs or []))  # posargs for test filtering.
+
+
+@nox.session
+def build(session):
+    """Minimal session to run build commands.
+
+    e.g., `nox -r -s build -- {{target}}`
+
+    """
+    toml = nox.project.load_toml("pyproject.toml")
+    build_deps = toml["project"]["optional-dependencies"]["build"]
+    session.install(*build_deps)  # but not the package itself...
+    session.run("python", "-m", "build_util", *(session.posargs or []))  # posargs for test filtering
+
+
+@nox.session
+def docs(session):
+    """Environment for doc prep.
+
+    See `build_util` for targets.
+
+    Use this session to run nbconvert utils.
+    # https://nbconvert.readthedocs.io/en/latest/usage.html#notebook-and-preprocessors
+
+    """
+    session.install("-e", ".[docs]")
+    if False:
+        toml = nox.project.load_toml("pyproject.toml")
+        pkg_deps = toml["project"]["optional-dependencies"]["docs"]
+        session.install(*pkg_deps)
+
+    session.run(*session.posargs)
